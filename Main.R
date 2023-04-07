7673fbd5e43761bc1a3f08883aa0e1e72db6f0e5
MaherAhmed0 <maher7ahmed985@gmail.com>
2023-04-08 01:54:38 +0200

    corr

--- Main.R
+++ Main.R
@@ -2,25 +2,17 @@
 # Import Data
 ##################################
 Main_DataFrame <- read.csv("train.csv",header = TRUE)
+
+Main_DataFrame$Id <- NULL
 ##################################
 # Exploring Data
 ##################################
-######################################################################################################
-colnames(Main_DataFrame)<-Main_DataFrame[1,]
-Main_DataFrame<-Main_DataFrame[-1,]
+cat("Dataset shape:(", dim(Main_DataFrame),")")
 
-rownames(Main_DataFrame)<-Main_DataFrame$Id
-Main_DataFrame<-Main_DataFrame[,-1]
-
-str(Main_DataFrame)
-######################################################################################################
-
-
-dim(Main_DataFrame)
-
+print("Dataset stucture:")
 str(Main_DataFrame)
 
-attributes(Main_DataFrame)
+names(Main_DataFrame)
 
 summary(Main_DataFrame)
 
@@ -30,10 +22,36 @@ plot(density(Main_DataFrame$SalePrice))
 ##################################
 # Cleaning Data
 ##################################
-print("Count of missing values by column wise")
+print("Missing or null values column wise")
 sapply(Main_DataFrame,function(x) sum(is.na(x)))
 
+NA_Cols <- which(colSums(is.na(Main_DataFrame)) > 0)
+sort(colSums(sapply(Main_DataFrame[NA_Cols], is.na)), decreasing = TRUE)
+cat('There are', length(NA_Cols), 'columns with missing values')
 
-#library(corrplot)
-#C <- cor(Main_DataFrame)
-#corrplot(C, method = "pie")
+##################################
+# Correlation on numeric features
+##################################
+Numeric_Data <- which(sapply(Main_DataFrame, is.numeric)) #index vector numeric variables
+# numericVarNames <- names(Numeric_Data) #saving names vector for use later on
+cat('There are', length(Numeric_Data), 'numeric variables')
+
+All_Num_Data <- Main_DataFrame[, Numeric_Data]
+Corr_Num_Data <- cor(All_Num_Data, use="pairwise.complete.obs")
+
+Data_Sorted <- as.matrix(sort(Corr_Num_Data[,'SalePrice'], decreasing = TRUE))
+
+# Select only high correlations
+High_Corr <- names(which(apply(Data_Sorted, 1, function(x) abs(x) > 0.5)))
+Corr_Table <- Corr_Num_Data[High_Corr, High_Corr]
+
+C <- cor(Corr_Table)
+
+corrplot(C, method="circle")
+
+corrplot(C, method="pie")
+
+corrplot(C, method="color")
+################################################################################################
+
+################################################################################################
